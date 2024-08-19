14364e57b54842558906b18ac24d4f17a3f8b16e
Alef oliveira <alefolivfeira@gmail.com>
2024-08-19 18:59:52 -0300

    add use debounci

--- src/pages/Checkout/components/CheckoutAddress/index.tsx
+++ src/pages/Checkout/components/CheckoutAddress/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { forwardRef, useCallback, useEffect, useState } from "react";
 
 import { useFormContext, Controller } from "react-hook-form";
